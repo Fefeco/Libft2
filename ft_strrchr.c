324454550518fc12753433516df68683d6616e14
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-27 13:35:21 +0100

    Correccion strrchr: Cambio en el valor de retorno para mantener igualdad con el man de la funcion original. Cambio en la condicion para que quede mas legible

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 13:04:10 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/22 10:47:37 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:07:39 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strrchr(const char *s, int c)
 	int	len;
 
 	len = ft_strlen(s);
-	while (len > -1)
+	while (len >= 0)
 	{
 		if ((char)c == *(s + len))
 			return ((char *)s + len);
 		--len;
 	}
-	return (0);
+	return (NULL);
 }
