f1a2c888b5f94ceb103e198161d91a4923b64103
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-27 13:23:17 +0100

    Correccion memset: Cambio en el nombre de variables para mantener igualdad con la funcion original. Cambio en el type de int a size_t para poder comparar en bucles

--- ft_memset.c
+++ ft_memset.c
@@ -6,24 +6,24 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 15:50:10 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/22 10:14:25 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/27 10:52:20 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int n, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	int				i;
-	unsigned char	c;
+	size_t			i;
+	unsigned char	ch;
 	char			*p;
 
 	i = 0;
-	c = (unsigned char)n;
+	ch = (unsigned char)c;
 	p = b;
-	while (i < (int)len)
+	while (i < len)
 	{
-		p[i] = c;
+		p[i] = ch;
 		i++;
 	}
 	return (b);
