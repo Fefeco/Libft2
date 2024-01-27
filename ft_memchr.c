1d486a7ffbbc7d1bead146674129fb24feb2a9b8
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-27 13:12:24 +0100

    Correccion memchr: Cambio nombre de variable y retorno para que mantenga correlacion con la funcion original

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,21 +6,21 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 14:23:20 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/22 10:11:01 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:12:59 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memchr(const void *s, int c, size_t size)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char	ch;
 	unsigned char	*p;
 
 	ch = (unsigned char)c;
 	p = (unsigned char *)s;
-	while (size--)
+	while (n--)
 		if (*p++ == ch)
 			return (--p);
-	return (0);
+	return (NULL);
 }
