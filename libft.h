1039123f7a3703e918a4dae8239099af85e0ebea
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-27 13:36:32 +0100

    Correccion libft.h: Cambio en las declaraciones de los prototipos de funciones para igualar las del los man de las funciones originales

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 10:13:46 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/24 12:12:28 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:54:11 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,24 @@
 # include <unistd.h>
 
 int			ft_atoi(const char *str);
-void		ft_bzero(void *s, size_t len);
-void		*ft_calloc(int coun, size_t size);
+void		ft_bzero(void *s, size_t n);
+void		*ft_calloc(size_t count, size_t size);
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
-void		*ft_memchr(const void *s, int c, size_t size);
-int			ft_memcmp(const void *s1, const void *s2, int size);
-void		*ft_memcpy(void *dst, const void *src, size_t len);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
 void		*ft_memmove(void *dst, const void *src, size_t len);
-void		*ft_memset(void *b, int n, size_t len);
+void		*ft_memset(void *b, int c, size_t len);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strdup(const char *s1);
-size_t		ft_strlcat(char *dst, const char *src, size_t dst_size);
-size_t		ft_strlcpy(char *dst, const char *src, size_t dst_size);
+size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t		ft_strlen(const char *s);
-int			ft_strncmp(const char *s1, const char *s2, size_t size);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char		*ft_strrchr(const char *s, int c);
 int			ft_tolower(int c);
