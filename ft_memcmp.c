97cdd3703de398ed030cd9bb8101dcee8eb14d3a
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-27 13:14:59 +0100

    Correcion memcmp: Cambio de type de int a size_t y nombre de variable para mantener igualdad con la funcion original. Inclui libft.h para que pueda interpretar el type size_t

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,18 +6,20 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 15:00:03 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/13 15:14:21 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/27 12:18:08 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_memcmp(const void *s1, const void *s2, int size)
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*ss1;
 	unsigned char	*ss2;
 
 	ss1 = (unsigned char *)s1;
 	ss2 = (unsigned char *)s2;
-	while (size--)
+	while (n--)
 		if ((*ss1++ - *ss2++) != 0)
 			return (*--ss1 - *--ss2);
 	return (0);
