ea2b864af2458f0fbd4fd9d8179b2865cade0887
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-28 22:58:35 +0100

    Correccion putendl_fd: Cambio en el type int a size_t. Agregue proteccion antes de usar st_strlen

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 11:51:08 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/24 12:00:46 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/28 21:13:31 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	ft_putendl_fd(char *s, int fd)
 {
-	int	len;
+	size_t	len;
 
-	len = ft_strlen(s);
+	if (s)
+		len = ft_strlen(s);
 	if (len)
 		write(fd, s, len);
 	write(fd, "\n", 1);
