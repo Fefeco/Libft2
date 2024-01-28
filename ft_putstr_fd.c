18974a591e680365390b8ce6ffb6ce7afe84a9b6
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-28 22:59:56 +0100

    Correccion putstr_fd: Cambio type int por sizt_t. Agregue proteccion antes de usar ft_strlen

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 10:33:37 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/24 11:11:44 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/28 21:01:31 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	len;
+	size_t	len;
 
-	len = ft_strlen(s);
+	if (s)
+		len = ft_strlen(s);
 	if (len)
 		write(fd, s, len);
 }
