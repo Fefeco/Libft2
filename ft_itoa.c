3fece51c239baa879f21d754a091de035e8c63f6
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-28 13:44:55 +0100

    Correccion itoa: Cambio el nombre de variables para mejor legibilidad. Cambio el type int por size_t donde es conveniente

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,23 +6,23 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 18:54:57 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/24 18:07:17 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/28 13:43:13 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_special_cases(char *rta, int n, int len)
+static void	ft_special_cases(char *ptr, int n, size_t len)
 {
 	if (n == 0)
-		ft_strlcpy(rta, "0", len + 1);
+		ft_strlcpy(ptr, "0", len + 1);
 	else
-		ft_strlcpy(rta, "-2147483648", len + 1);
+		ft_strlcpy(ptr, "-2147483648", len + 1);
 }
 
-static int	ft_ilen(int n)
+static size_t	ft_ilen(int n)
 {
-	int	len;
+	size_t	len;
 
 	if (n == 0)
 		return (1);
@@ -44,28 +44,28 @@ static int	ft_ilen(int n)
 
 char	*ft_itoa(int n)
 {
-	char	*rta;
-	int		len;
+	char	*ptr;
+	size_t	len;
 
 	len = ft_ilen(n);
-	rta = (char *)malloc(sizeof(char) * (len + 1));
-	if (!rta)
-		return (rta);
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (ptr);
 	if (n == -2147483648 || n == 0)
 	{
-		ft_special_cases(rta, n, len);
-		return (rta);
+		ft_special_cases(ptr, n, len);
+		return (ptr);
 	}
 	if (n < 0)
 	{
 		n *= -1;
-		rta[0] = '-';
+		ptr[0] = '-';
 	}
-	rta[len] = '\0';
+	ptr[len] = '\0';
 	while (n != 0)
 	{
-		rta[--len] = '0' + (n % 10);
+		ptr[--len] = '0' + (n % 10);
 		n /= 10;
 	}
-	return (rta);
+	return (ptr);
 }
