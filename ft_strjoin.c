21d58b0bdc9573d08296825706283ee1b07d3413
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-28 11:34:50 +0100

    Correccion strjoin: Cambio en el nombre de las variables para que sea mas legible. Elimine la proteccion de numeros negativos porque mi funcion devuelve 0 o numeros positivos. Cambio en el type de int a size_t

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 20:49:22 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/17 21:09:51 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/28 11:29:00 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*ptr;
-	int		len;
-	int		s1len;
+	size_t	total_len;
+	size_t	s1_len;
 
-	s1len = ft_strlen(s1);
-	len = s1len + ft_strlen(s2);
-	if (len < 1)
-		len = 0;
-	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	total_len = s1_len + ft_strlen(s2);
+	ptr = (char *)malloc((total_len + 1) * sizeof(char));
 	if (!ptr)
 		return (ptr);
-	ft_strlcpy(ptr, s1, s1len + 1);
-	ft_strlcat(ptr, s2, len + 1);
+	ft_strlcpy(ptr, s1, s1_len + 1);
+	ft_strlcat(ptr, s2, total_len + 1);
 	return (ptr);
 }
