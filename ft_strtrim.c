8e1bc9a62f0fbda540d3e9caed69aba3c228e581
Federico Carranza <fcarranz@student.42barcelona.com>
2024-01-28 13:16:21 +0100

    Correccion strtrim: Cambio en los tipos de datos de int a size_t. Agreugue la condicion que no reste end si es 0 ya que no deberia trabajar con negativos. Cambie tambien el nombre de la variable para mayor legibilidad

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: fcarranz <fcarranz@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 09:30:28 by fcarranz          #+#    #+#             */
-/*   Updated: 2024/01/25 19:18:50 by fcarranz         ###   ########.fr       */
+/*   Updated: 2024/01/28 13:07:06 by fcarranz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int		start;
-	int		end;
-	char	*ret;
+	size_t		start;
+	size_t		end;
+	char		*str;
 
-	if (!s1 || !set || !*s1 || !*set)
+	if (!s1 || !set)
 		return (NULL);
 	start = 0;
 	end = ft_strlen(s1);
 	while (ft_strchr(set, s1[start]) && start < end)
 		++start;
-	while (ft_strchr(set, s1[end - 1]) && (end - 1) > start)
+	while (ft_strchr(set, s1[end - 1]) && (end - 1) > start && end != 0)
 		--end;
-	ret = (char *)malloc(sizeof(char) * ((end + 1) - start));
-	if (!ret)
-		return (ret);
-	ft_strlcpy(ret, s1 + start, ((end + 1) - start));
-	return (ret);
+	str = (char *)malloc(sizeof(char) * ((end - start) + 1));
+	if (!str)
+		return (str);
+	ft_strlcpy(str, s1 + start, ((end + 1) - start));
+	return (str);
 }
